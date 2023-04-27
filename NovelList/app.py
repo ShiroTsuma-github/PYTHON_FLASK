2ffabf9c5b16e13d69bb2c14557b64fc726d5650
ShiroTsuma-github <spam.shiro@gmail,com>
2023-04-27 20:18:16 +0200

    update with displaying

--- NovelList/app.py
+++ NovelList/app.py
@@ -19,7 +19,9 @@ indexVariables = {
 @app.route('/')
 def index():
     books = Book.query.all()
-    return render_template('Lista.html', books=books, **indexVariables)
+
+    app.logger.info(BookType.query.all())
+    return render_template('Lista.html', books=books,**indexVariables)
     # return('testing')
 
 
@@ -36,16 +38,14 @@ def add_book():
         priority = request.form.get('priority')
         book_type = BookType.query.filter_by(name=book_type).first()
         status = Status.query.filter_by(name=status).first()
-
-        book = Book(title=title, book_type=book_type, position=position, last_chapter=last_chapter,
+        book = Book(title=title, book_type=book_type, last_chapter=last_chapter,
                    total_chapters=total_chapters, frequency=frequency, status=status, notes=notes,
                    priority=priority)
         db.session.add(book)
         db.session.commit()
-        return redirect(url_for('book_list'))
+        return redirect(url_for('index'))
 
-    print(BookType.query.all())
-    app.logger.info(BookType.query.all())
+    
     book_types = [book_type.name for book_type in BookType.query.all()]
     statuses = [status.name for status in Status.query.all()]
 
@@ -55,7 +55,16 @@ def add_book():
 @app.route('/book_list')
 def book_list():
     books = Book.query.all()
-    return render_template('book_list.html', books=books)
+    return render_template('Lista.html', books=books)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+#TODO: Add edit book functionality
+#TODO: Add delete book functionality
+#TODO: Add search functionality
+#TODO: Add sorting functionality
+#TODO: Add filtering functionality
+#TODO: Add links to books
+#TODO: Add link field to book
+#TODO: Import from json
