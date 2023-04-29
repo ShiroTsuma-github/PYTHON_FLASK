7d6fa363ad355d611ded778629ba5a0b9056aec7
ShiroTsuma-github <spam.shiro@gmail,com>
2023-04-29 15:21:58 +0200

    small change to form and added option to edit and remove books

--- NovelList/app.py
+++ NovelList/app.py
@@ -20,8 +20,26 @@ indexVariables = {
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if request.form.get('actionused') == 'edit':
-             pass
+        pass
+    book_types = [book_type.name for book_type in BookType.query.all()]
+    statuses = [status.name for status in Status.query.all()]
+    books = Book.query.all()
+    return render_template('Lista.html',
+                           books=books,
+                           book_types=book_types,
+                           statuses=statuses,
+                           **indexVariables)
+
+
+@app.route('/add', methods=['POST'])
+def add_book():
+    if request.method == 'POST':
+        if request.form.get('delete') == 'delete':
+
+            book_id = int(request.form.get('bookid'))
+            delete_book(book_id)
+        elif request.form.get('actionused') == 'edit':
+            edit_book(request.form)
         else:
             title = request.form.get('title')
             book_type = request.form.get('book_type')
@@ -41,43 +59,7 @@ def index():
             db.session.commit()
             return redirect(url_for('index'))
 
-    
-    book_types = [book_type.name for book_type in BookType.query.all()]
-    statuses = [status.name for status in Status.query.all()]
-    books = Book.query.all()
-    return render_template('Lista.html',
-                           books=books,
-                           book_types=book_types,
-                           statuses=statuses,
-                           **indexVariables)
-
-
-@app.route('/add', methods=['GET', 'POST'])
-def add_book():
-    if request.method == 'POST':
-        title = request.form.get('title')
-        book_type = request.form.get('book_type')
-        last_chapter = request.form.get('last_chapter')
-        total_chapters = request.form.get('total_chapters')
-        link = request.form.get('link')
-        grade = request.form.get('grade')
-        status = request.form.get('status')
-        notes = request.form.get('notes')
-        priority = request.form.get('priority')
-        book_type = BookType.query.filter_by(name=book_type).first()
-        status = Status.query.filter_by(name=status).first()
-        book = Book(title=title, book_type=book_type, last_chapter=last_chapter,
-                total_chapters=total_chapters, grade=grade, link=link, status=status, notes=notes,
-                priority=priority)
-        db.session.add(book)
-        db.session.commit()
-        return redirect(url_for('index'))
-
-     
-    book_types = [book_type.name for book_type in BookType.query.all()]
-    statuses = [status.name for status in Status.query.all()]
-
-    return render_template('add.html', book_types=book_types, statuses=statuses)
+    return redirect(url_for('index'))
 
 @app.route('/get_book_details')
 def get_book_details():
@@ -100,11 +82,54 @@ def get_book_details():
     }
     return jsonify(book_details)
 
+
+def delete_book(book_id):
+    book = Book.query.filter_by(id=book_id).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+def edit_book(form):
+    book_id = int(form['bookid'])
+    title = form['title']
+    book_type = form['book_type']
+    status = form['status']
+    total_chapters = int(form['total_chapters'])
+    last_chapter = form['last_chapter']
+    grade = 0 if form['grade'] == '' else int(form['grade'])
+    priority = int(form['priority'])
+    link = form['link']
+    notes = form['notes']
+
+    
+    # Call the update_book function with the form data and book_id
+    update_book(book_id, title, book_type, status, total_chapters, last_chapter, grade, priority, link, notes)
+    # Redirect back to the main page
+    return redirect(url_for('index'))
+
+def update_book(book_id, title, book_type, status, total_chapters, last_chapter, grade, priority, link, notes):
+    book = Book.query.filter_by(id=book_id).first()
+    if book:
+        # Update the fields with the new values
+        book.title = title
+        book.book_type = BookType.query.filter_by(name=book_type).first()
+        book.status = Status.query.filter_by(name=status).first()
+        book.total_chapters = int(total_chapters)
+        book.last_chapter = last_chapter
+        book.grade = grade
+        book.priority = priority
+        book.link = link
+        book.notes = notes
+        # Commit the changes to the database
+        db.session.commit()
+        return 'Record updated successfully!'
+    else:
+        return 'Record not found'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
-#TODO: Add edit book functionality
-#TODO: Add delete book functionality
-#TODO: Add search functionality
 #TODO: Add sorting functionality
 #TODO: Add filtering functionality
+#TODO: Change the way the data is displayed
+#TODO: Add buttons near each book to edit and delete it
